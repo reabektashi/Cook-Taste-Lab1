15f74ffcebdf366f573b4a2bb2f413edc2734dae
AnidaKadriu <anidaakadriu@gmail.com>
2025-11-16 22:10:27 +0100

    .env file changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import RecipeTable from './pages/RecipeTable';
-import UserTable from './pages/UserTable';
-import InsertUser from './pages/InsertUser';
-import InsertRecipe from './pages/InsertRecipe';
-import UpdateUser from './pages/UpdateUser';
-import UpdateRecipe from './pages/UpdateRecipe';
-import DeleteUser from "./pages/DeleteUser";
-import DeleteRecipe from "./pages/DeleteRecipe";
 import Layout from './components/Layout';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -19,7 +11,6 @@ import Desserts from "./pages/Recipes/dessert";
 import AboutUs from './pages/aboutus';
 import Logout from './pages/logout';
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import Terms from "./pages/Terms";
 import Dinner from "./pages/Recipes/dinner"; 
 
 export default function App() {
@@ -40,15 +31,7 @@ export default function App() {
           <Route path="logout" element={<Logout />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="privacy" element={<PrivacyPolicy />} />
-          <Route path="terms" element={<Terms />} />
-          <Route path="usertable" element={<UserTable />} />
-          <Route path="recipetable" element={<RecipeTable />} />
-          <Route path="insert-user" element={<InsertUser />} />
-          <Route path="insert-recipe" element={<InsertRecipe />} />
-          <Route path="update-user/:id" element={<UpdateUser />} />
-          <Route path="update-recipe/:id" element={<UpdateRecipe />} />
-          <Route path="delete-user/:id" element={<DeleteUser />} />
-          <Route path="delete-recipe/:id" element={<DeleteRecipe />} />
+    
         </Route>
       </Routes>
     </BrowserRouter>
